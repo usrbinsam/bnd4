0b986fd2ff585cdfdc2a39a12dd2a8e1210777e5
Sam Hoffman <sam@codewizardshq.com>
2025-07-10 17:43:34 -0400

    examples ... forgot what I was doing here?

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,22 @@
-use bnd4::BND4File;
+use bnd4::{encodings, BND4File, CipherMode};
 use std::fs::File;
+use std::io::{Cursor, Seek};
 
 fn main() {
-    let mut sl2_file = File::open("tests/NR.bnd4").unwrap();
-    let bnd4data = BND4File::from_file(&mut sl2_file).unwrap();
+    let mut sl2_file = File::open("tests/DSR.bnd4").unwrap();
+    let mut bnd4data = BND4File::from_file(&mut sl2_file).unwrap();
 
-    for entry in bnd4data.entries.iter() {
+    for entry in bnd4data.entries.iter_mut() {
         println!("{:?}", entry.name);
+        entry
+            .decrypt(
+                CipherMode::CBC,
+                &hex::decode("0123456789ABCDEFFEDCBA9876543210").unwrap(),
+            )
+            .unwrap();
+        let mut cursor = Cursor::new(&entry.data);
+        cursor.seek(std::io::SeekFrom::Start(0x00000118)).unwrap();
+        println!("{:?}", encodings::read_utf16(&mut cursor).unwrap());
+        println!("data offset: {}", entry.header.data_offset);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-mod encodings;
+pub mod encodings;
 mod entry;
 mod entry_header;
 mod errors;
 mod file;
 mod header;
 
+pub use entry::CipherMode;
 pub use errors::*;
 pub use file::BND4File;
